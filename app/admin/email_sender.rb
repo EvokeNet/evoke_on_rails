40f0561d83fd8d8e3a092f55997f4813a9f9dfab
Guilherme de Oliveira Fernandes <guilherme_oliveira55@hotmail.com>
2016-11-30 16:17:31 +0000

    Email sending on active admin

--- app/admin/email_sender.rb
+++ app/admin/email_sender.rb
@@ -1,5 +1,22 @@
 ActiveAdmin.register_page "EmailSender" do
   content do
-    render partial: 'email_sender'
+    @mail = Mail.new do
+    end
+    render partial: 'email_sender', :mail => @mail
   end
+  
+  page_action :send_email, method: :post do
+    if (params[:mail]).present? 
+      @mail = Mail.new do
+        from "evokenettech@gmail.com"
+      end
+      @mail.delivery_method :smtp, :address => "smtp.mailtrap.io", :port => 2525 , :domain => 'mailtrap.io', :user_name => '80f5f1aef4ae15', :password => '8db4cd615a4d6b', :authentication => :plain, :enable_starttls_auto => true, :ssl =>false
+      @mail.to = params[:mail]['to']
+      @mail.subject = params[:mail]['subject']
+      @mail.body = params[:mail]['body']
+      @mail.deliver
+      redirect_to admin_emailsender_path, notice: "Your email has been delivered"
+    end
+  end
+
 end
