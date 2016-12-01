e38eb7b6de932fd698de666ef9549896128c804f
Guilherme de Oliveira Fernandes <guilherme_oliveira55@hotmail.com>
2016-12-01 16:53:56 +0000

    working email sending

--- app/admin/email_sender.rb
+++ app/admin/email_sender.rb
@@ -1,32 +1,25 @@
-ActiveAdmin.register_page "EmailSender" do
+ActiveAdmin.register_page "Email Sender" do
   content do
-    
-    if (params[:mail]).present? 
-      @mail = MailValidator.new(params[:to])
-    else
-      @mail = MailValidator.new()
-    end
+    @mail = MailValidator.new()
     render partial: 'email_sender', :mail => @mail
   end
   
+  page_action :send_email, method: :get do
+    redirect_to admin_emailsender_path
+  end
   
   page_action :send_email, method: :post do
     if (params[:mail]).present? 
-      @mail = MailValidator.new do
-        from "evokenettech@gmail.com"
-      end
-      #@mail.delivery_method :smtp, :address => "smtp.mailtrap.io", :port => 2525 , :domain => 'mailtrap.io', :user_name => '80f5f1aef4ae15', :password => '8db4cd615a4d6b', :authentication => :plain, :enable_starttls_auto => true, :ssl =>false
-      @mail.to = params[:mail]['to']
-      @mail.subject = params[:mail]['subject']
-      @mail.body = params[:mail]['body']
-      @mail.validate
-      #@mail.deliver
-      redirect_to admin_emailsender_path, notice: "Your email has been delivered" unless @mail.errors.any?
+      mail_params = params.require(:mail).permit(:from, :to, :subject, :body)
+      @mail = MailValidator.new(mail_params)
+      @mail.from = "evokenettech@gmail.com"
+      @mail.deliver
+      redirect_to admin_emailsender_path, notice: "Your email has been delivered" and return unless @mail.errors.any? 
       @mail.errors.full_messages.each do |msg|
-        #flash[:error] = msg
+        flash[:error] = msg
       end
-      render action: 'index', :mail => @mail, :layout => false
     end
+    render action: 'index', :mail => @mail, :layout => false
   end
 
 end
