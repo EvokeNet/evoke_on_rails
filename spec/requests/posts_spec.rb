d26a97a6dfd212e7a00350eef72c2ac6721c667c
Guilherme de Oliveira Fernandes <guilherme_oliveira55@hotmail.com>
2016-12-07 18:23:23 +0000

    started post top corner button

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
     it "works! (now write some real specs)" do
       get posts_path
-      #expect(response).to have_http_status(200)
-      expect(response).to redirect_to(:controller => 'posts', :action => 'index')
+      expect(response).to have_http_status(200)
     end
   end
 end
