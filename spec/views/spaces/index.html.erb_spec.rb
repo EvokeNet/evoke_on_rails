64489a7340b13875d3041de20959466299be6ea4
Guilherme de Oliveira Fernandes <guilherme_oliveira55@hotmail.com>
2016-12-08 12:44:21 +0000

    fix spaces index rspec issue

--- spec/views/spaces/index.html.erb_spec.rb
+++ spec/views/spaces/index.html.erb_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
+require 'rspec/rails'
+require 'devise'
 
 RSpec.describe "spaces/index", type: :view do
 
@@ -6,12 +8,16 @@ RSpec.describe "spaces/index", type: :view do
   include Warden::Test::Helpers
   
   before(:each) do
+    
     @user = assign(:user, User.create!(
       :name => "New",
       :lastname => "User",
       :password => "password",
       :email => "user@example.com"
     ))
+    
+    current_user = @user
+    sign_in current_user
 
     assign(:spaces, [
       Space.create!(
@@ -33,7 +39,6 @@ RSpec.describe "spaces/index", type: :view do
     render
     assert_select "tr>td", :text => "Name".to_s, :count => 2
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "", :count => 2
     assert_select "tr>td", :text => "New".to_s, :count => 2
   end
 end
